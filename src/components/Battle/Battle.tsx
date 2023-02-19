34bc435f37f2034751b438a1e383f692a6252c9f
RusakevichMR <rusakevichmr@gmail.com>
2023-02-19 12:33:08 +0200

    deploy

--- src/components/Battle/Battle.tsx
+++ src/components/Battle/Battle.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 type unutType = {
   heals: number;
   defense: number;
-  demage: number;
+  damage: number;
   initiative: number;
   name: string;
   type: string;
@@ -14,21 +14,32 @@ type enemysType = unutType[];
 const player = {
   heals: 100,
   defense: 2,
-  demage: 10,
+  damage: 10,
   initiative: 15,
   name: "player",
   type: "player",
   id: 1,
 };
 
+type logsType = {
+  attacker: unutType;
+  defender: unutType;
+  damage: number;
+  newHP: number;
+  oldHP: number;
+  dead: boolean;
+};
+
 export const Battle = () => {
-  const handleAttack = () => {};
+  const [logs, setLogs] = useState<Array<logsType | null | undefined>>([]);
+  const [move, setMove] = useState(1);
+
   const [unit, setUnit] = useState([
     player,
     {
       heals: 40,
       defense: 4,
-      demage: 5,
+      damage: 5,
       initiative: 5,
       name: "orcs",
       type: "enemy",
@@ -37,7 +48,7 @@ export const Battle = () => {
     {
       heals: 12,
       defense: 2,
-      demage: 5,
+      damage: 5,
       initiative: 12,
       name: "goblin",
       type: "enemy",
@@ -46,7 +57,7 @@ export const Battle = () => {
     {
       heals: 12,
       defense: 1,
-      demage: 5,
+      damage: 5,
       initiative: 15,
       name: "goblin2",
       type: "enemy",
@@ -57,10 +68,10 @@ export const Battle = () => {
   const [queue, setQueue] = useState<unutType[]>([]);
 
   useEffect(() => {
-    console.log("aaa333");
-
-    setQueue(queueCreate({ arr: unit }));
-  }, []);
+    if (queue.length === 0) {
+      setQueue(queueCreate({ arr: unit }));
+    }
+  }, [move]);
 
   useEffect(() => {
     const clone = JSON.parse(JSON.stringify(queue));
@@ -74,10 +85,18 @@ export const Battle = () => {
       }
     }
     if (index > 0) {
-      setQueue(clone.slice(index, clone.length));
+      updateQueue(clone, index);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queue]);
+
+  const updateQueue = (clone: unutType[], index: number) => {
+    const newQueue = clone.slice(index, clone.length);
+    setQueue(newQueue);
+    if (newQueue.length === 0) {
+      setMove(move + 1);
+    }
+  };
 
   const Attack = ({
     attackerId,
@@ -90,15 +109,31 @@ export const Battle = () => {
     const attacker = unit[attackerIndex];
     const defenderIndex = unit.map((e) => e.id).indexOf(defenderID);
     const defender = unit[defenderIndex];
-    if (attacker?.heals <= 0) return;
-    const damage = attacker.demage - defender.defense;
+    if (attacker?.heals <= 0 || defender?.heals <= 0 || !attacker || !defender)
+      return;
+    const damage = attacker.damage - defender.defense;
 
     setUnit((prev) => {
       const new1 = prev
         .map((e) => {
           if (e.id === defenderID) {
             const newHels = e.heals - damage;
+
+            setLogs((prev) => {
+              return [
+                ...prev,
+                {
+                  attacker: attacker,
+                  defender: defender,
+                  damage: damage,
+                  newHP: newHels,
+                  oldHP: e.heals,
+                  dead: newHels <= 0 ? true : false,
+                },
+              ];
+            });
             if (newHels <= 0) return null;
+
             return { ...e, heals: newHels };
           }
           return e;
@@ -106,7 +141,6 @@ export const Battle = () => {
         .filter(function (el) {
           return el != null;
         });
-      console.log(new1, "new1");
       return new1 as any;
     });
   };
@@ -116,28 +150,47 @@ export const Battle = () => {
   return (
     <div>
       <h1>Battle</h1>
+      <h2>Ход {move}</h2>
       <p>
         {unit[playerIndex].name}: {unit[playerIndex].heals} HP
       </p>
       <ul>
         {unit.map((enemy, index) => {
-          if (enemy.type === "player") return <div key={index}></div>;
+          if (enemy.type === "player" || enemy.heals <= 0)
+            return <div key={`${index}`}></div>;
           return (
-            <li key={index}>
-              <p>
-                {enemy.name}: {enemy.heals} HP
-              </p>
-              <button
-                onClick={() => {
-                  Attack({ attackerId: player.id, defenderID: enemy.id });
-                }}
-              >
-                Attack
-              </button>
-            </li>
+            <div key={`${index}`}>
+              <li>
+                <p>
+                  {enemy.name}: {enemy.heals} HP
+                </p>
+                <button
+                  onClick={() => {
+                    Attack({ attackerId: player.id, defenderID: enemy.id });
+                    const clone = JSON.parse(JSON.stringify(queue));
+                    updateQueue(clone, 1);
+                  }}
+                >
+                  Attack
+                </button>
+              </li>
+            </div>
           );
         })}
       </ul>
+      <div>
+        {logs?.map((e, index) => {
+          return (
+            <div key={index}>
+              <div>
+                {e?.attacker.name} атакует {e?.defender.name}
+              </div>
+              <div>{`${e?.oldHP}|${e?.newHP}`}</div>
+              {e?.dead && <div key={index}>{e?.defender.name} мертв</div>}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
